0d5d790ef45e39173b2d49f8826e1541ca9de916
张泽宇 <zhangzeyu@lztogether.com>
2017-07-24 19:47:43 +0800

    修复热更新

--- webapp-music/src/components/recommend/recommend.vue
+++ webapp-music/src/components/recommend/recommend.vue
@@ -27,7 +27,7 @@
         getRecommend().then((res) => {
           if (res.code === ERR_OK) {
             this.recommends = res.data.slider
-            console.log(1, res)
+            console.log(res)
           }
         })
       }
